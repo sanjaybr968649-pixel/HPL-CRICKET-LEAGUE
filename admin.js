21aea3d782c084312940994fea34d1192168ce3a
sanjaybr968649-pixel <sanjay.br968649@gmail.com>
2026-09-21 21:26:33 +0530

    Add Firebase configuration to admin.js

---
+++ admin.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDW1MP5MBKoz6oOHxeWWHF7Ed8UGlsJsQ8",
+  authDomain: "hpl-cricket-league.firebaseapp.com",
+  projectId: "hpl-cricket-league",
+  storageBucket: "hpl-cricket-league.firebasestorage.app",
+  messagingSenderId: "310274398112",
+  appId: "1:310274398112:web:4122ce31fb352d4f05b6c5",
+  measurementId: "G-PPMME44H28"
+};
